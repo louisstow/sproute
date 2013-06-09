441b1ff3382229c20e92b4d331bca0c46c4914e5
louisstow <louisstow@gmail.com>
2013-06-09 13:34:51 +1000

    * Remove shit

--- hooks.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	"get": function (block) {
-		console.log("GET", block);
-		return "FUCK OFF"
-	}
-}
--- test.js
+++
@@ -1,8 +0,0 @@
-var storage = require("./storage").init("myblog");
-var _ = require("underscore");
-
-storage.onready = function () {
-	this.get("data/posts", function (err, q) {
-		console.log("QUERY", q)
-	})
-};
