8c00e5988d78418ce022816691353db1a11c8b63
louisstow <louisstow@gmail.com>
2013-12-01 13:45:44 +1000

    * List new directories in listing

--- test/fs-test.js
+++ test/fs-test.js
@@ -2,25 +2,32 @@ var fs = require("../lib/RiakFS");
 fs.init({name: "test"});
 
 var cb = console.log.bind(console);
-fs.exists("view/test.sprt", function (exists) {
-	console.log("EXISTS", exists);
+// fs.exists("view/test.sprt", function (exists) {
+// 	console.log("EXISTS", exists);
 
-	if (exists) {
-		fs.readFile("view/test.sprt", function  (err, data) {
-			console.log("READFILE", err, data);
-			fs.unlink("view/test.sprt", cb);
-		});
+// 	if (exists) {
+// 		fs.readFile("view/test.sprt", function  (err, data) {
+// 			console.log("READFILE", err, data);
+// 			fs.unlink("view/test.sprt", cb);
+// 		});
 
-		fs.readdir("view", function (err, list) {
-			console.log("READDIR", err, list);
-		})
-	} else {
+// 		fs.readdir("view", function (err, list) {
+// 			console.log("READDIR", err, list);
+// 		})
+// 	} else {
 		
-		fs.writeFile("view/test.sprt", "BLAH", cb);
+// 		fs.writeFile("view/test.sprt", "BLAH", cb);
+// 	}
+// });
+
+
+fs.exists("view/default", function (exists) {
+	console.log("EXISTS", exists)
+	if (exists) {
+		fs.rmdir("view/default", cb);
+	} else {
+		fs.mkdir("view/default", cb);
 	}
 });
-
-
-
 
 //fs.readdir("views", cb);
